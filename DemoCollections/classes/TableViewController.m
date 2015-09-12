fbf7083892201e7a2d2898312e2a41199830f878
王俊仁 <jr-wong@qq.com>
2015-09-13 02:26:22 +0800

    sdfsdf

--- DemoCollections/classes/TableViewController.m
+++ DemoCollections/classes/TableViewController.m
@@ -20,6 +20,8 @@
 
 @interface TableViewController ()
 
+@property (nonatomic, strong) NSArray *classArray;
+
 @end
 
 @implementation TableViewController
@@ -27,23 +29,24 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 500)];
-    view.hidden = YES;
-    self.tableView.tableFooterView = view;
+
+    _classArray = @[
+                    @{@"QTSelectionView" : [QTTestViewController class]},
+                    @{@"QTTagListView" : [QTTagListTestViewController class]},
+                    @{@"QTPickerView" : [QTPickerViewController class]},
+                    @{@"DynamicDemo" : [DynamicController class]},
+                    @{@"QTActionSheet" : [QTActionSheetViewController class]},
+                    @{@"QTRecommendView" : [RecommendTestViewController class]},
+                    @{@"QTAlertView" : [QTAlertViewViewController class]},
+                    @{@"TEST" : [TestViewController class]},
+                    ];
 
 }
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    NSLog(@"%f,%f", self.tableView.contentSize.height,self.view.height - 64);
-}
-
 
 #pragma mark - Table view data source
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 10;
+    return self.classArray.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -54,114 +57,16 @@
     {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:ID];
     }
-    cell.textLabel.text = [NSString stringWithFormat:@"%zd", indexPath.row];
 
-    switch (indexPath.row) {
-        case 0:
-        {
-            cell.textLabel.text = @"QTSelectionView";
-        }break;
-
-        case 1:
-        {
-            cell.textLabel.text = @"QTTagListView";
-        }break;
-
-        case 2:
-        {
-            cell.textLabel.text = @"QTPickerView";
-        }break;
-
-        case 3:
-        {
-            cell.textLabel.text = @"DynamicDemo";
-        }break;
-            
-        case 4:
-        {
-            cell.textLabel.text = @"QTActionSheet";
-        }break;
-        case 5:
-        {
-            cell.textLabel.text = @"QTRecommendView";
-        }break;
-        case 6:
-        {
-            cell.textLabel.text = @"QTAlertView";
-        }break;
-        case 7:
-        {
-            cell.textLabel.text = @"TEST";
-        }break;
-        default:
-            break;
-    }
+    cell.textLabel.text = [self.classArray[indexPath.row] allKeys].lastObject;
 
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    switch (indexPath.row) {
-        case 0:
-        {
-            QTTestViewController *tc = [[QTTestViewController alloc] init];
-            [self.navigationController pushViewController:tc animated:YES];
-        }break;
-
-        case 1:
-        {
-            QTTagListTestViewController *tc = [[QTTagListTestViewController alloc] init];
-
-            [self.navigationController pushViewController:tc animated:YES];
-        }break;
-
-        case 2:
-        {
-            QTPickerViewController *pv = [[QTPickerViewController alloc] init];
-
-            [self.navigationController pushViewController:pv animated:YES];
-        }break;
-
-        case 3:
-        {
-            DynamicController *dc = [[DynamicController alloc] init];
-
-            [self.navigationController pushViewController:dc animated:YES];
-        }break;
-            
-        case 4:
-        {
-            QTActionSheetViewController *dc = [[QTActionSheetViewController alloc] init];
-            
-            [self.navigationController pushViewController:dc animated:YES];
-        }break;
-            
-        case 5:
-        {
-            RecommendTestViewController *dc = [[RecommendTestViewController alloc] init];
-            
-            [self.navigationController pushViewController:dc animated:YES];
-        }break;
-            
-        case 6:
-        {
-            QTAlertViewViewController *dc = [[QTAlertViewViewController alloc] init];
-            
-            [self.navigationController pushViewController:dc animated:YES];
-        }break;
-            
-        case 7:
-        {
-            TestViewController *dc = [[TestViewController alloc] init];
-            
-            [self.navigationController pushViewController:dc animated:YES];
-        }break;
-
-        default:
-            break;
-    }
-
+    UIViewController *controller = [[((Class)[self.classArray[indexPath.row] allValues].lastObject) alloc] init];
+    [self.navigationController pushViewController:controller animated:YES];
 }
 
 @end
